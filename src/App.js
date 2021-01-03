adcad3d6161be98adc938d2f405721d3db4aa507
HyunIn Sim <>
2021-01-03 11:18:42 +0900

    add ContextAPI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import {createGlobalStyle} from 'styled-components'
-import TodoTemplate from './Components/TodoTemplate'
-import TodoHead from './Components/TodoHead'
-import TodoList from './Components/TodoList'
-import TodoCreate from './Components/TodoCreate'
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./Components/TodoTemplate";
+import TodoHead from "./Components/TodoHead";
+import TodoList from "./Components/TodoList";
+import TodoCreate from "./Components/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 const GlobalStyle = createGlobalStyle`
   body{
     background: #e9ecef;
   }
-`
-
+`;
 
 const App = () => {
   return (
-    <>
-    <GlobalStyle/>
-    <TodoTemplate>
-      <TodoHead/>
-      <TodoList />
-      <TodoCreate />
-    </TodoTemplate>
-    </>
-  )
-}
+    <TodoProvider>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </TodoProvider>
+  );
+};
 
-export default App
+export default App;
